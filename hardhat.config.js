ed9fbad66b8d1cb563cee431e73ecebdb384cd39
curiosiT <54553419+jepeal85@users.noreply.github.com>
2022-03-30 12:50:03 +0200

    Implementation of ERC20 token

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require("dotenv").config({ path: __dirname + "/.env" });
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.4",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
